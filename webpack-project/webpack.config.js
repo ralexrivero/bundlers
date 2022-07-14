58b8f6ea5ddaf86815e80862e31d014e9a58b8cb
ralexrivero <ralexrivero@gmail.com>
2022-07-14 18:46:42 -0300

    initial webpack setup

---
+++ webpack-project/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: './dist'
+        }
+    }
+}
