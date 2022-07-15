dabde8d3e73368b97f6985356480b2e96548ff69
ralexrivero <ralexrivero@gmail.com>
2022-07-14 21:01:07 -0300

    eslint

--- webpack-project/webpack.config.js
+++ webpack-project/webpack.config.js
@@ -11,5 +11,21 @@ module.exports = {
         static: {
             directory: './dist'
         }
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        },
+        {
+            test: /\.(.js|.jsx)$/,
+            exclude: /node_modules/,
+            use: ['eslint-loader']
+        }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     }
 }
